aef01e753cb1e6d710d8748c015692eed2013f79
AvaLilac <257690424+AvaLilac@users.noreply.github.com>
2026-07-26 15:41:03 -0400

    Implement loadInject function for plugin loading
    Signed-off-by: AvaLilac <257690424+AvaLilac@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,97 @@
+import * as fs from "fs";
+import * as path from "path";
 import { IUpdateInfo, updateElectronApp } from "update-electron-app";
-
 import { BrowserWindow, Notification, app, shell } from "electron";
 import started from "electron-squirrel-startup";
-
 import { config } from "./native/config";
 import { initDiscordRpc } from "./native/discordRpc";
 import { initTray } from "./native/tray";
 import { BUILD_URL, createMainWindow, mainWindow } from "./native/window";
-
 // Squirrel-specific logic
 // create/remove shortcuts on Windows when installing / uninstalling
 // we just need to close out of the app immediately
 if (started) {
   app.quit();
 }
-
 // disable hw-accel if so requested
 if (!config.hardwareAcceleration) {
   app.disableHardwareAcceleration();
 }
-
 // ensure only one copy of the application can run
 const acquiredLock = app.requestSingleInstanceLock();
-
 const onNotifyUser = (_info: IUpdateInfo) => {
   const notification = new Notification({
     title: "Update Available",
     body: "Restart the app to install the update.",
     silent: true,
   });
-
   notification.show();
+};
+
+const loadInject = () => {
+  if (!mainWindow) return;
+
+  mainWindow.webContents.on("dom-ready", async () => {
+    try {
+      const plugins: string[] = [
+        "titlebar.js",
+        "css.js"
+      ];
+
+      for (const plugin of plugins) {
+        const pluginPath: string = path.join(__dirname, plugin);
+        const pluginCode: string = fs.readFileSync(pluginPath, "utf8");
+        await mainWindow.webContents.executeJavaScript(pluginCode, true);
+      }
+    } catch {
+      /* empty */
+    }
+  });
 };
 
 if (acquiredLock) {
   // start auto update logic
   updateElectronApp({ onNotifyUser });
-
   // create and configure the app when electron is ready
   app.on("ready", () => {
     // create window and application contexts
     createMainWindow();
-
+    loadInject();
     // save first launch state
     if (config.firstLaunch) {
       // Doesn't do anything right now. Used to enable auto start, but that behaviour was removed.
       // Left in case it gets used in the future.
       config.firstLaunch = false;
     }
-
     initTray();
     initDiscordRpc();
-
     // Windows specific fix for notifications
     if (process.platform === "win32") {
       app.setAppUserModelId("chat.stoat.notifications");
     }
   });
-
   // focus the window if we try to launch again
   app.on("second-instance", () => {
     mainWindow.show();
     mainWindow.restore();
     mainWindow.focus();
   });
-
   // macOS specific behaviour to keep app active in dock:
   // (irrespective of the minimise-to-tray option)
-
   app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
       app.quit();
     }
   });
-
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow();
+      loadInject();
     } else {
       mainWindow.show();
       mainWindow.focus();
     }
   });
-
   // ensure URLs launch in external context
   app.on("web-contents-created", (_, contents) => {
     // prevent navigation out of build URL origin
@@ -91,7 +100,6 @@ if (acquiredLock) {
         event.preventDefault();
       }
     });
-
     // handle links externally
     contents.setWindowOpenHandler(({ url }) => {
       if (
@@ -103,7 +111,6 @@ if (acquiredLock) {
           shell.openExternal(url);
         });
       }
-
       return { action: "deny" };
     });
   });
